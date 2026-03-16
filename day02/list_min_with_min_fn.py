eb789ec5729b914928e9cd37b9adba000f771894
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-16 23:25:36 +0530

    Added program for minimum of the list using min()

---
+++ day02/list_min_with_min_fn.py
@@ -0,0 +1,13 @@
+#Finding minimum in a list
+#Concept: using for loop and user input
+
+
+static_list = []
+
+for num in range(3):
+    user_input = int(input(f"Enter the list element {num+1} : "))
+    static_list.append(user_input)
+
+for digit in static_list:
+    minimum = min(static_list)
+print(f"Min of the list is: {minimum}")
