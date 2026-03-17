701bf75b8ce164c522e85f7c95fba4788c36f9f9
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-17 19:35:05 +0530

    Reverse of a list using reverse() fn

---
+++ day02/list_reverse_reverse_fn.py
@@ -0,0 +1,14 @@
+#Reverse a list
+
+
+static_list = []
+
+for num in range(3):
+    user_input = int(input(f"Enter the list element {num+1} : "))
+    static_list.append(user_input)
+print(f"User list is : {static_list}")
+
+
+for i in static_list:
+    reverse_list = list(reversed(static_list))
+print(f"Reverse of the list is: {reverse_list}")
