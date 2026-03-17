eb33748999b93e9a2a7e43fa63f007e816aa3375
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-18 00:04:27 +0530

    Program to find negative number count in the list

---
+++ day03/count_negative_num.py
@@ -0,0 +1,18 @@
+#  Count negative numbers
+#  Concept : using loop
+
+static_list = []
+
+for i in range(4):
+    user_input = int(input(f"Enter the list element {i+1}: "))
+    static_list.append(user_input)
+print(f"User entered: {static_list}")
+
+# assigning a count variable for negative number count
+
+count = 0
+
+for element in static_list:
+    if element < 0:
+        count += 1
+print(f"Total number of positive elements in the list: {count}")
