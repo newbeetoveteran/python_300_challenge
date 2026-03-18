c3709de13451448a8a50f3c98826d930a793905b
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-18 23:19:30 +0530

    Merging a list by .extend

--- day04/merging_by_extend.py
+++ day04/merging_by_extend.py
@@ -3,7 +3,7 @@
 
 user_list1 = []
 for i in range(4):
-    user_input1 = int(input(f"Enter the elements in list {i+1}: "))
+    user_input1 = int(input(f"Enter element {i+1} for list 1: "))
     user_list1.append(user_input1)
 print(f"User entered: {user_list1}")
 
@@ -11,7 +11,7 @@ print(f"User entered: {user_list1}")
 
 user_list2 = []
 for i in range(3):
-    user_input2 = int(input(f"Enter the elements in list {i+1}: "))
+    user_input2 = int(input(f"Enter element {i+1} list 2: "))
     user_list2.append(user_input2)
 print(f"User entered: {user_list2}")
 
