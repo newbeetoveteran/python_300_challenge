eb47a04f1da73db4b85dc80b3a72165b29752638
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-19 23:30:22 +0530

    Creating a nested list from user input

--- day05/creating_nested_list.py
+++ day05/creating_nested_list.py
@@ -1,15 +1,24 @@
 # Flatten a nested list
 # Concept : append() and loops
 
-# creating a nested list
+# taking user input for size of the list
 
 list_size = int(input("Enter size of the list: "))
 user_list = []
+
+# looping through the main list based on user defined size
+
 for i in range(list_size):
     user_sublist = []
     sublist_size = int(input(f"Enter the size of sublist {i+1}: "))
+
+# looping through sublist and appending the element value
+
     for j in range(sublist_size):
         user_input = int(input(f"Enter the {j+1} element: "))
         user_sublist.append(user_input)
     user_list.append(user_sublist)
+
+# Printing the final nested list
+
 print(f"Nested loop: {user_list}")
