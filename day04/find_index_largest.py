03d505e1d30e031f955bb4f9314f151add30d11a
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-19 14:29:13 +0530

    Finding max and its index in a list with enumerate

---
+++ day04/find_index_largest.py
@@ -0,0 +1,21 @@
+#  Find index of largest
+#  Concept: Using max() and loop
+
+user_list = []
+for i in range(4):
+    user_input = int(input(f"Enter element {i+1} for list 1: "))
+    user_list.append(user_input)
+print(f"User entered: {user_list}")
+
+# Finding max() 
+
+max_list = max(user_list)
+
+# Enumerating the user list and finding index of the variable num
+# enumerate() is used when index and variable value are required at the same time
+# Gives first max() index in a list with repeating max element
+
+for index, num in enumerate(user_list):
+    if num == max_list:
+        break
+print(f"Index of largest number in the list is: {index}")
