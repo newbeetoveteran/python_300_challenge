36a9921f9bc297b34cf8411ddc34ebd71838cf5e
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-23 14:38:16 +0530

    Checking if a list1 is subset of list2- using isubset

---
+++ day06/subset_check.py
@@ -0,0 +1,30 @@
+#  Subset check
+#  Concept: list conversion to set. Using issubset method
+
+
+list_size1 = int(input("Enter the size of list 1: "))
+list_size2 = int(input("Enter the size of list 2: "))
+
+list1 = []
+list2 = []
+
+if list_size1 <= 0 or list_size2 <= 0:
+    print(f"List is empty. Please enter correct list size.")
+else:
+    for i in range(list_size1):
+        user_input1 = int(input(f"Enter {i+1} element: "))
+        list1.append(user_input1)
+    print(f"Entered list 1: {list1}")
+    
+    for j in range(list_size2):
+        user_input2 = int(input(f"Enter {j+1} element: "))
+        list2.append(user_input2)
+    print(f"Entered list 2: {list2}")
+
+    set1 = set(list1)
+    set2 = set(list2)
+
+    if set1.issubset(set2):
+        print(f"{set1} is subset of {set2}")
+    else:
+        print(f"{set1} is not subset {set2}")
