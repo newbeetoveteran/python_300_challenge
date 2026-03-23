4527e3c1f7ba6caac6564fdee89cf08d7fdeda18
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-23 09:20:54 +0530

    Finding set intersection

---
+++ day06/find_intersection_set.py
@@ -0,0 +1,29 @@
+#  Find intersection
+#  Concept: sets and .intersection()
+
+set_size1 = int(input("Enter size of the set1: "))
+set_size2 = int(input("Enter size of the set2: "))
+
+static_set1 = set()
+static_set2 = set()
+
+if set_size1 == 0:
+    print("Set is empty. Please enter correct set size for set 1.")
+else:
+    for i in range(set_size1):
+        user_input1 = int(input(f"Enter the {i+1} element for set 1: "))
+        static_set1.add(user_input1)
+    print(f"First set: {static_set1}")
+
+if set_size2 == 0:
+    print("Set is empty. Please enter correct set size for set 2.")
+
+else:
+    for j in range(set_size2):
+        user_input2 = int(input(f"Enter the {j+1} element for set 2: "))
+        static_set2.add(user_input2)
+    print(f"Second set: {static_set2}")
+
+intersection_set = static_set1.intersection(static_set2)
+
+print(f"Intersection of 2 sets: {intersection_set}")
