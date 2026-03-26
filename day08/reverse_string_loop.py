765851cd94afdf897cf1a3d738cb083fa15e7178
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-26 23:39:05 +0530

    Reversing a string using loop

--- day08/reverse_string_loop.py
+++ day08/reverse_string_loop.py
@@ -5,5 +5,8 @@ string_1 = "This is a string"
 reversed_string = ""
 
 for char in string_1:
+
+    # using char in front of reversed string to rather than reversed string + char
+    
     reversed_string = char + reversed_string
 print(f"Reversed string: {reversed_string}")
