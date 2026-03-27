aafcc962ae8966e150143364a1f31a45d2bdcf53
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-27 19:07:50 +0530

    Converting string-lowercase

---
+++ day08/lower_case.py
@@ -0,0 +1,7 @@
+#  Convert to lowercase
+#  Concept: String and .lower()
+
+user_input = input("Enter the sentence: ")
+
+lower_case = user_input.lower()
+print(f"Lower case of sentence is: {lower_case}")
